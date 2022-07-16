91f89cd06c83dd05f960b8d0c30143dac8594a53
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-07-16 11:55:24 -0300

    parametriza cadastro de mensalista

---
+++ projetoEstacionamento/src/tst/TesteCadastraMensalista.java
@@ -0,0 +1,54 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import app.Mensalistas;
+
+@RunWith(Parameterized.class)
+public class TesteCadastraMensalista {
+	private Mensalistas mensalistas;
+	
+	private String placas[];
+	
+	@Before
+	public void setup() {
+		mensalistas = new Mensalistas();
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParameters(){
+		Object[][] parametros = new Object[][] {
+			{new String[] {"ABC-1234", "ABC-4321"}},
+			{new String[] {"ABC-1234", "ABC-4321", "CAB-4321"}}, 
+			{new String[] {"ABC-1234", "ABC-4321", "CAB-4321", "CAB-1234"}}, 
+		};
+		
+		return Arrays.asList(parametros);
+	}
+	
+	public TesteCadastraMensalista(String strings[]) {
+		this.placas = strings;
+	}
+	
+	@Test
+	public void cadastraMensalista() {
+		for (int i = 0; i < placas.length-1; i++) {
+			mensalistas.cadastraMensalista(placas[i]);
+		}
+		
+		for (int i = 0; i < placas.length-1; i++) {
+			assertTrue(mensalistas.ehMensalista(placas[i]));
+		}
+		
+		assertFalse(mensalistas.ehMensalista(placas[placas.length-1]));
+	}
+}
