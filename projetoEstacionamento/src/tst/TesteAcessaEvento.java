a0dac136a811f06ccb86a0110a42db55fe3e0d98
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-07-16 15:40:55 -0300

    Teste acessa evento parametrizado

--- projetoEstacionamento/src/tst/TesteAcessaEvento.java
+++ projetoEstacionamento/src/tst/TesteAcessaEvento.java
@@ -2,17 +2,41 @@ package tst;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.Eventos;
 
+@RunWith(Parameterized.class)
 public class TesteAcessaEvento {
 	private Eventos eventos;
+	
+	private String dados[][];
 	
 	@Before
 	public void setup() {
 		eventos = new Eventos();
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParameters(){
+		Object[][] parametros = new Object[][] {
+			{new String[][] {{"FlaXFlu", "9:00", "13:00", "17/07/2022"}}},
+			{new String[][] {{"FlaXFlu", "9:00", "13:00", "17/07/2022"}, {"FlaXCam", "19:00", "23:00", "20/07/2022"}}},
+			{new String[][] {{"FlaXFlu", "9:00", "13:00", "17/07/2022"}, {"FlaXCam", "19:00", "23:00", "20/07/2022"}, {"FlaXVas", "14:00", "18:00", "17/07/2022"}}}
+		};
+		
+		return Arrays.asList(parametros);
+	}
+	
+	public TesteAcessaEvento(String d[][]) {
+		this.dados = d;
 	}
 
 	@Test
@@ -21,40 +45,17 @@ public class TesteAcessaEvento {
 		assertTrue(eventos.existeEvento("FLAXFLU", "17/07/2022"));
 		assertEquals(eventos.getHorarioInicio("FLAXFLU", "17/07/2022"), "9:00");
 		assertEquals(eventos.getHorarioFim("FLAXFLU", "17/07/2022"), "13:00");
+		
+		for (String s[] : dados) {
+			eventos.cadastraEvento(s[0], s[1], s[2], s[3]);
+		}
+		
+		for (String s[] : dados) {
+			assertTrue(eventos.existeEvento(s[0], s[3]));
+			
+			assertEquals(eventos.getHorarioInicio(s[0], s[3]), s[1]);
+			assertEquals(eventos.getHorarioFim(s[0], s[3]), s[2]);
+		}
+		
 	}
-	
-	@Test
-	public void testaExisteDoisEventos() {
-		eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022");
-		eventos.cadastraEvento("FlaXCam", "19:00", "23:00", "20/07/2022");
-		
-		assertTrue(eventos.existeEvento("FLAXFLU", "17/07/2022"));
-		assertTrue(eventos.existeEvento("FLAXCAM", "20/07/2022"));
-		
-		assertEquals(eventos.getHorarioInicio("FLAXFLU", "17/07/2022"), "9:00");
-		assertEquals(eventos.getHorarioFim("FLAXFLU", "17/07/2022"), "13:00");
-		
-		assertEquals(eventos.getHorarioInicio("FLAXCAM", "20/07/2022"), "19:00");
-		assertEquals(eventos.getHorarioFim("FLAXCAM", "20/07/2022"), "23:00");
-	}
-	
-	@Test
-	public void testaExisteTresEventos() {
-		eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022");
-		eventos.cadastraEvento("FlaXCam", "19:00", "23:00", "20/07/2022");
-		eventos.cadastraEvento("FlaXVas", "14:00", "18:00", "17/07/2022");
-		
-		assertTrue(eventos.existeEvento("FLAXFLU", "17/07/2022"));
-		assertTrue(eventos.existeEvento("FLAXCAM", "20/07/2022"));
-		assertTrue(eventos.existeEvento("flaxvas", "17/07/2022"));
-		
-		assertEquals(eventos.getHorarioInicio("FLAXFLU", "17/07/2022"), "9:00");
-		assertEquals(eventos.getHorarioInicio("FLAXCAM", "20/07/2022"), "19:00");
-		assertEquals(eventos.getHorarioInicio("flaxvas", "17/07/2022"), "14:00");
-		
-		assertEquals(eventos.getHorarioFim("FLAXFLU", "17/07/2022"), "13:00");		
-		assertEquals(eventos.getHorarioFim("FLAXCAM", "20/07/2022"), "23:00");
-		assertEquals(eventos.getHorarioFim("flaxvas", "17/07/2022"), "18:00");
-	}
-
 }
