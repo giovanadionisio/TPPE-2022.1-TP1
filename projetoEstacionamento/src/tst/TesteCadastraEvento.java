6035370c95474694bfbef880eab4df943394b477
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-07-16 12:28:14 -0300

    teste cadastro evento triangulado

--- projetoEstacionamento/src/tst/TesteCadastraEvento.java
+++ projetoEstacionamento/src/tst/TesteCadastraEvento.java
@@ -19,5 +19,20 @@ public class TesteCadastraEvento {
 	public void cadastraEvento() {
 		assertTrue(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
 	}
+	
+	@Test
+	public void cadastraDoisEventos() {
+		assertTrue(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
+		assertFalse(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
+		assertTrue(eventos.cadastraEvento("FlaXCam", "9:00", "13:00", "17/07/2022"));
+	}
+	
+	@Test
+	public void cadastraTresEventos() {
+		assertTrue(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
+		assertFalse(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
+		assertTrue(eventos.cadastraEvento("FlaXCam", "9:00", "13:00", "17/07/2022"));
+		assertTrue(eventos.cadastraEvento("FlaXVas", "14:00", "18:00", "17/07/2022"));
+	}
 
 }
