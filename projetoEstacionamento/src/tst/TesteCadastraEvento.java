a461322b102300ff8b9e4cfba4bce134ada4a4f6
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-07-16 12:40:31 -0300

    teste cadastro evento parametrizado

--- projetoEstacionamento/src/tst/TesteCadastraEvento.java
+++ projetoEstacionamento/src/tst/TesteCadastraEvento.java
@@ -2,37 +2,47 @@ package tst;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.Eventos;
 
+@RunWith(Parameterized.class)
 public class TesteCadastraEvento {
 	private Eventos eventos;
+	private String dados[][];
 	
 	@Before
 	public void setup() {
 		eventos = new Eventos();
 	}
+	
+	@Parameters 
+	public static Collection<Object[]> getParameters(){
+		Object[][] parametros = new Object [][] {
+			{new String[][] {{"FlaXFlu", "9:00", "13:00", "17/07/2022"}}},
+			{new String[][] {{"FlaXFlu", "9:00", "13:00", "17/07/2022"}, {"FlaXCam", "9:00", "13:00", "17/07/2022"}}},
+			{new String[][] {{"FlaXFlu", "9:00", "13:00", "17/07/2022"}, {"FlaXCam", "9:00", "13:00", "17/07/2022"}, {"FlaXVas", "14:00", "18:00", "17/07/2022"}}}
+		};
+		
+		return Arrays.asList(parametros);
+	}
+	
+	public TesteCadastraEvento(String d[][]) {
+		this.dados = d;
+	}
 
 	@Test
-	public void cadastraEvento() {
-		assertTrue(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
-	}
-	
-	@Test
-	public void cadastraDoisEventos() {
-		assertTrue(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
-		assertFalse(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
-		assertTrue(eventos.cadastraEvento("FlaXCam", "9:00", "13:00", "17/07/2022"));
-	}
-	
-	@Test
-	public void cadastraTresEventos() {
-		assertTrue(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
-		assertFalse(eventos.cadastraEvento("FlaXFlu", "9:00", "13:00", "17/07/2022"));
-		assertTrue(eventos.cadastraEvento("FlaXCam", "9:00", "13:00", "17/07/2022"));
-		assertTrue(eventos.cadastraEvento("FlaXVas", "14:00", "18:00", "17/07/2022"));
+	public void cadastraEventos() {
+		for (String s[] : dados) {
+			assertTrue(eventos.cadastraEvento(s[0], s[1], s[2], s[3]));
+		}
 	}
 
 }
