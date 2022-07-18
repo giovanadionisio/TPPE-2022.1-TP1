69d55f5ba184980cd6ccd1207b3626e25443a1f1
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2022-07-18 20:49:13 -0300

    Teste por duplicação - cadastro hora cheia

--- projetoEstacionamento/src/tst/CadastroAcessoHoraCheiaTeste.java
+++ projetoEstacionamento/src/tst/CadastroAcessoHoraCheiaTeste.java
@@ -17,12 +17,22 @@ public class CadastroAcessoHoraCheiaTeste {
 	}
 
 	@Test
-	public void test() {
+	public void test1() {
 		acessoHoraCheia.cadastraAcessoHoraCheia("G49NG",30.0f, "8:30", "9:30", 15);
 		assertTrue(acessoHoraCheia.contemAcessoHoraCheia("G49NG"));
-		assertEquals(acessoHoraCheia.getValorFracao("G49NG"), 30.0f, 0.f);
+		assertEquals(acessoHoraCheia.getValorFracao("G49NG"), 30.0f, 0.1f);
 		assertEquals(acessoHoraCheia.getHorarioEntrada("G49NG"), "8:30");
 		assertEquals(acessoHoraCheia.getHorarioSaida("G49NG"),  "9:30");
 		assertEquals(acessoHoraCheia.getPercentual("G49NG"),  15, 0.1f);
+	}
 
+	@Test
+	public void test2() {
+		acessoHoraCheia.cadastraAcessoHoraCheia("AC50M",20.0f, "8:00", "11:00", 10);
+		assertTrue(acessoHoraCheia.contemAcessoHoraCheia("AC50M"));
+		assertEquals(acessoHoraCheia.getValorFracao("AC50M"), 20.0f, 0.1f);
+		assertEquals(acessoHoraCheia.getHorarioEntrada("AC50M"), "8:00");
+		assertEquals(acessoHoraCheia.getHorarioSaida("AC50M"),  "11:00");
+		assertEquals(acessoHoraCheia.getPercentual("AC50M"),  10, 0.1f);
+	}
 }
