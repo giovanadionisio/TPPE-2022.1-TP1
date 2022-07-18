b9ec8cc6689a8755a5a6af279167baeacc344d16
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-07-18 17:56:33 -0300

    teste de cadastro de estacionamento triangulado

--- projetoEstacionamento/src/tst/TesteCadastraEstacionamento.java
+++ projetoEstacionamento/src/tst/TesteCadastraEstacionamento.java
@@ -36,6 +36,18 @@ public class TesteCadastraEstacionamento {
 		
 		assertEquals(this.programa.estacionamentos.size(), 2);
 	}
+	
+	@Test
+	public void testeCadastraTresEstacionamentos() throws ValorAcessoInvalidoException, DescricaoEmBrancoException {
+		Estacionamento estacionamento = new Estacionamento(30.0f, 0.15f, 120f, 0.45f, 600f, 50f, "06:00", "22:00", 300, 0.5f);
+		Estacionamento estacionamento2 = new Estacionamento(20.0f, 0.1f, 70f, 0.3f, 455f, 60f, "00:00", "00:00", 120, 0.6f);
+		Estacionamento estacionamento3 = new Estacionamento(10.0f, 0f, 50f, 0.4f, 350f, 40f, "06:00", "22:00", 600, 0.7f);
+		
+		this.programa.cadastraEstacionamento(estacionamento);
+		this.programa.cadastraEstacionamento(estacionamento2);
+		
+		assertEquals(this.programa.estacionamentos.size(), 2);
+	}
 
 
 }
