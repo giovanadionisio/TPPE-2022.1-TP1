757cc40102d732a964b7859853ccc4fabb38cefa
Eduarda Servidio Claudino <52542729+ServidioEC@users.noreply.github.com>
2022-07-17 23:56:14 -0300

    Cadastro de acesso de veiculos

--- projetoEstacionamento/src/app/Veiculo.java
+++ projetoEstacionamento/src/app/Veiculo.java
@@ -1,5 +1,32 @@
 package app;
 
-public class Veiculo {
+import java.util.regex.Pattern;
 
+import exceptions.*;
+
+public class Veiculo {
+	  private String placa;
+    public Horario horario;
+
+    public String getPlaca() {
+        return this.placa;
+    }
+    
+    public void cadastraAcesso(String horarioEntrada, String horarioSaida, String placa) throws ValorAcessoInvalidoException, DescricaoEmBrancoException{
+    	
+    	this.horario = new Horario(horarioEntrada,horarioSaida);
+    	
+    	placa = placa.trim();
+        if(placa.isEmpty()) {
+            throw new DescricaoEmBrancoException("Placa");
+        }
+        
+        //formato placa "antiga"
+        Pattern pattern = Pattern.compile("^[A-z]{3}-[0-9]{4}$");
+        if(!pattern.matcher(placa).find()) {
+            throw new ValorAcessoInvalidoException("Placa");
+        } else {
+            this.placa = placa.toUpperCase();
+        }
+    }
 }
