5549f9094f417bf6d55e8dd14397a8a1b132fe8a
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-07-18 19:32:09 -0300

    Menu de cadastro de estacionamento
    Co-authored-by: hryds <rodriguerya@gmail.com>

--- projetoEstacionamento/src/app/Programa.java
+++ projetoEstacionamento/src/app/Programa.java
@@ -19,19 +19,15 @@ public class Programa {
 
     public Programa() throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
         this.estacionamentos = new ArrayList<Estacionamento>();
-        this.cadastraEstacionamento(new Estacionamento(30.0f, 0.15f, 120f, 0.45f, 600f, 50f, "06:00", "22:00", 300, 0.5f));
+        /*this.cadastraEstacionamento(new Estacionamento(30.0f, 0.15f, 120f, 0.45f, 600f, 50f, "06:00", "22:00", 300, 0.5f));
         this.cadastraEstacionamento(new Estacionamento(20.0f, 0.1f, 70f, 0.3f, 455f, 60f, "00:00", "00:00", 120, 0.6f));
-        this.cadastraEstacionamento(new Estacionamento(10.0f, 0f, 50f, 0.4f, 350f, 40f, "06:00", "22:00", 600, 0.7f));
-//    	this.mensalistas = new Mensalistas();
-//    	this.cadastraEstacionamentos();
-//        this.boasVindas();
+        this.cadastraEstacionamento(new Estacionamento(10.0f, 0f, 50f, 0.4f, 350f, 40f, "06:00", "22:00", 600, 0.7f));*/
+    	this.mensalistas = new Mensalistas();
     }
 
 	public void cadastraEstacionamento(Estacionamento estacionamento) {
 		this.estacionamentos.add(estacionamento);
 	}
-    
-    
 
     private void boasVindas() throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
         while (this.running) {
@@ -48,8 +44,7 @@ public class Programa {
 
             switch (opcao) {
                 case 1:
-                    // this.estac1.menu();
-                	//this.menuOpcoes();
+                    this.cadastrarEstacionamento();
                     break;
                 case 2:
                     this.selecionaEstacionamento();
@@ -67,29 +62,63 @@ public class Programa {
         }
     }
     
-    private void visualizarEstacionamentos() {
-    	for (int i = 0; i < estacionamentos.size(); i++) {
-			System.out.printf("Estacionamento %d\n", i+1);
-			System.out.printf("Valor fração: %f\n", estacionamentos.get(i).valorFracao);
-			System.out.printf("Valor hora cheia: %f\n", estacionamentos.get(i).valorHoraCheia);
-			System.out.printf("Valor diária diurna: %f\n", estacionamentos.get(i).valorDiaria);
-			System.out.printf("Valor diária noturna: %f\n", estacionamentos.get(i).valorDiariaNoturna);
-			System.out.printf("Valor acesso mensalista: %f\n", estacionamentos.get(i).valorMensalista);
-			System.out.printf("Valor acesso evento: %f\n", estacionamentos.get(i).valorEvento);
-			System.out.printf("Horario Abertura: %s\n", estacionamentos.get(i).horarioAbre);
-			System.out.printf("Horario Fechamento: %s\n", estacionamentos.get(i).horarioFecha);
-			System.out.printf("Capacidade: %d\n", estacionamentos.get(i).capacidade);
-			System.out.printf("retorno contratante: %f\n", estacionamentos.get(i).retornoContratante);
-			System.out.println("*******************************************************************\n\n");
-			
+    private void cadastrarEstacionamento() throws ValorAcessoInvalidoException, DescricaoEmBrancoException {
+    	 System.out.println("----------------------------------------------------");
+         System.out.println("|          Cadastro de estacionamento!             |");
+         System.out.println("----------------------------------------------------\n");
+         
+         System.out.println("Infome a capacidade do estacionamento:");
+         int capacidade = input.nextInt();
+         System.out.println("Infome o valor da fração de hora (R$):");
+         float valorFracao = Float.parseFloat(input.next());
+         System.out.println("Infome o valor da hora cheia (R$):");
+         float valorHoraCheia = Float.parseFloat(input.next());
+         System.out.println("Infome o valor da diária diurna (R$):");
+         float valorDiaria = Float.parseFloat(input.next());
+         System.out.println("Infome o percentual da diária noturna aplicado sobre o valor da diária diurna (%):");
+         float valorDiariaNoturna = Float.parseFloat(input.next());
+         System.out.println("Infome o valor do acesso mensalista (R$):");
+         float valorMensalista = Float.parseFloat(input.next());
+         System.out.println("Infome o valor do acesso por evento (R$):");
+         float valorEvento = Float.parseFloat(input.next());
+         System.out.println("Infome o horário de abertura do estacionamento:");
+         String horarioAbre = input.next();
+         System.out.println("Infome o horário de fechamento do estacionamento:");
+         String horarioFecha = input.next();
+         System.out.println("Infome o valor repassado ao contratante:");
+         float retornoContratante = Float.parseFloat(input.next());
+         
+         this.cadastraEstacionamento(new Estacionamento(valorFracao, valorHoraCheia, valorDiaria, valorDiariaNoturna, valorMensalista, valorEvento, horarioAbre, horarioFecha, capacidade, retornoContratante));
+         System.out.println("Estacionamento Cadastrado!\n\n");
+         
+         this.boasVindas();
+    }
+
+	private void visualizarEstacionamentos() {
+		if(this.estacionamentos.size() == 0) {
+			System.out.println("Nenhum estacionamento cadastrado!\n\n");
+		} else {	
+			for (int i = 0; i < estacionamentos.size(); i++) {
+				System.out.printf("Estacionamento %d\n", i+1);
+				System.out.printf("Valor fração: %f\n", estacionamentos.get(i).valorFracao);
+				System.out.printf("Valor hora cheia: %f\n", estacionamentos.get(i).valorHoraCheia);
+				System.out.printf("Valor diária diurna: %f\n", estacionamentos.get(i).valorDiaria);
+				System.out.printf("Valor diária noturna: %f\n", estacionamentos.get(i).valorDiariaNoturna);
+				System.out.printf("Valor acesso mensalista: %f\n", estacionamentos.get(i).valorMensalista);
+				System.out.printf("Valor acesso evento: %f\n", estacionamentos.get(i).valorEvento);
+				System.out.printf("Horario Abertura: %s\n", estacionamentos.get(i).horarioAbre);
+				System.out.printf("Horario Fechamento: %s\n", estacionamentos.get(i).horarioFecha);
+				System.out.printf("Capacidade: %d\n", estacionamentos.get(i).capacidade);
+				System.out.printf("retorno contratante: %f\n", estacionamentos.get(i).retornoContratante);
+				System.out.println("*******************************************************************\n\n");
+				
+			}
 		}
-		
 	}
 
 	private void selecionaEstacionamento() throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
 		if(this.estacionamentos.size() == 0) {
 			System.out.println("Nenhum estacionamento cadastrado!\n\n");
-			this.boasVindas();
 		} else {
 			for (int i = 0; i < estacionamentos.size(); i++) {
 				System.out.printf("%d - Estacionamento %d\n",i+1, i+1);
@@ -101,11 +130,6 @@ public class Programa {
 			this.menuOpcoes();
 		}
 	}
-
-	public static void main(String[] args) throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
-        Programa programa = new Programa();
-        programa.boasVindas();
-    }
     
     private void menuOpcoes() throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
     	System.out.println("----------------------------------------------------");
@@ -162,6 +186,10 @@ public class Programa {
     	}
 	}
     
+    public static void main(String[] args) throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
+        Programa programa = new Programa();
+        programa.boasVindas();
+    }
     /*
 	public void calculaAcesso() {
         // (valores do objeto de acesso)
