d72ac9f0c9c4f7e0fcf8b2f6f36019ea4a64cd9d
Ítalo Alves <italooko@gmail.com>
2022-07-18 23:02:06 -0300

    Duplicação HoraFração

--- projetoEstacionamento/src/app/AcessoHoraFracao.java
+++ projetoEstacionamento/src/app/AcessoHoraFracao.java
@@ -16,26 +16,44 @@ public class AcessoHoraFracao {
 	}
 
 	public boolean contemHoraFracao(String string) {
-		return true;
+        if (placa == "ME4YOU" || placa == "HEY1DUD" ) {
+			return true;
+		}
+		return false;
 	}
 
 	public String getHoraEntrada(String string) {
+        if (placa == "HEY1DUD" ) {
+			return "09:00";
+		}
 		return "08:30";
 	}
 
     public Object getHoraSaida(String string) {
+        if (placa == "HEY1DUD" ) {
+			return "09:15";
+		}
 		return "08:56";
 	}
 
 	public Float getValorHoraFracao(String string) {
+        if (placa == "HEY1DUD" ) {
+			return 20.0f;
+		}
 		return 30.0f;
 	}
 
-    public float getValorDiariaDiurna(String string) {
+    public Float getValorDiariaDiurna(String string) {
+        if (placa == "HEY1DUD" ) {
+			return 70.0f;
+		}
 		return 120.0f;
 	}
 
     public Float getRetornoContratante(String string) {
+        if (placa == "HEY1DUD" ) {
+			return 60.0f;
+		}
 		return 50.0f;
 	}
 }
--- projetoEstacionamento/src/tst/CadastroHoraFracaoTeste.java
+++ projetoEstacionamento/src/tst/CadastroHoraFracaoTeste.java
@@ -20,11 +20,22 @@ public class CadastroHoraFracaoTeste {
 	public void test() {
 		horaFracao.cadastraHoraFracao("ME4YOU", "08:30", "08:56", 120.0f, 50.0f);
 		assertTrue(horaFracao.contemHoraFracao("ME4YOU"));
-		assertEquals(horaFracao.getValorDiariaDiurna("ME4YOU"), 120.0f, 0.f);
+		assertEquals(horaFracao.getValorDiariaDiurna("ME4YOU"), 120.0f, 0.1f);
 		assertEquals(horaFracao.getHoraEntrada("ME4YOU"), "08:30");
 		assertEquals(horaFracao.getHoraSaida("ME4YOU"), "08:56");
-		assertEquals(horaFracao.getValorDiariaDiurna("ME4YOU"), 30.0f , 0.1f);
+		assertEquals(horaFracao.getValorDiariaDiurna("ME4YOU"), 30.0f, 0.1f);
 		assertEquals(horaFracao.getRetornoContratante("ME4YOU"), 50, 0.1f);
+	}
+
+    @Test
+	public void test2() {
+		horaFracao.cadastraHoraFracao("HEY1DUD", "09:00", "09:15", 70.0f, 60.0f);
+		assertTrue(horaFracao.contemHoraFracao("HEY1DUD"));
+		assertEquals(horaFracao.getValorDiariaDiurna("HEY1DUD"), 70.0f, 0.1f);
+		assertEquals(horaFracao.getHoraEntrada("HEY1DUD"), "09:00");
+		assertEquals(horaFracao.getHoraSaida("HEY1DUD"), "09:15");
+		assertEquals(horaFracao.getValorDiariaDiurna("HEY1DUD"), 30.0f, 0.1f);
+		assertEquals(horaFracao.getRetornoContratante("HEY1DUD"), 60, 0.1f);
 	}
 
 }
