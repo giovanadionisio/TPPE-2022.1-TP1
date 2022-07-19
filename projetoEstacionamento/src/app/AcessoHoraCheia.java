c3522e0b1b37afc310bbb3be712b841e9f499742
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2022-07-18 21:27:02 -0300

    Triangulação por parametrização - Acesso Hora Cheia

--- projetoEstacionamento/src/app/AcessoHoraCheia.java
+++ projetoEstacionamento/src/app/AcessoHoraCheia.java
@@ -1,63 +1,88 @@
 package app;
 
+import java.util.ArrayList;
+
 public class AcessoHoraCheia {
 
-	private String placa;
-	private Float valorFracao;
-	private String horarioEntrada; 
-	private String horarioSaida; 
-	private Float percentual;
+	ArrayList<String> placa;
+	ArrayList<Float> valorFracao;
+	ArrayList<Float> percentual;
+	ArrayList<String> horarioEntrada;
+	ArrayList<String> horarioSaida;
+	
+	public AcessoHoraCheia(){
+		this.placa = new ArrayList<String>();
+		this.valorFracao = new ArrayList<Float>();
+		this.percentual = new ArrayList<Float>();
+		this.horarioEntrada = new ArrayList<String>();
+		this.horarioSaida = new ArrayList<String>();
+	}
 
 	public void cadastraAcessoHoraCheia(String placa, float valorFracao, String horarioEntrada, String horarioSaida, float percentual) {
-		this.placa = placa;
-		this.valorFracao = valorFracao;
-		this.horarioEntrada = horarioEntrada;
-		this.horarioSaida = horarioSaida;
-		this.percentual = percentual;
+		this.placa.add(placa);
+		this.valorFracao.add(valorFracao);
+		this.horarioEntrada.add(horarioEntrada);
+		this.horarioSaida.add(horarioSaida);
+		this.percentual.add(percentual);
 	}
 	
-	public boolean contemAcessoHoraCheia(String string) {
-		if(placa == "G49NG" || placa == "AC50M" ) {
-			return true;
+	public boolean contemAcessoHoraCheia(String placa) {
+		boolean resposta = false; 
+		for (String p:this.placa) {
+			if (p.equalsIgnoreCase(placa))
+				return true;
 		}
-		return false; 
+		return resposta; 
 	}
 
-	public float getValorFracao(String string) {
-		if (placa == "G49NG" ) {
-			return 30.0f;
+	public float getValorFracao(String placa) {
+		int pos= 0;
+		for (String p:this.placa) {
+			if (p.equalsIgnoreCase(placa))
+				pos = p.indexOf(placa);
 		}
-		return 20.0f;
-	}
-	
-	public Float getValorAcessoHoraCheia(String string) {
-		if (placa == "G49NG" ) {
-			return 102.0f;
-		}
-		return 216.0f;
+		return this.valorFracao.get(pos);
 	}
 
-	public String getHorarioEntrada(String string) {
-		if (placa == "G49NG" ) {
-			return "8:30";
+	public Float getValorAcessoHoraCheia(String placa) {
+		int pos= 0;
+		for (String p:this.placa) {
+			if (p.equalsIgnoreCase(placa))
+				pos = p.indexOf(placa);
 		}
-		return "8:00";
+		
+		float valor = (this.valorFracao.get(pos)*4) * (1 - (this.percentual.get(pos)/100.0f));
+		
+		return valor;
 	}
 
-	public Float getPercentual(String string) {
-		if (placa == "G49NG" ) {
-			return 15.0f;
+	public String getHorarioEntrada(String placa) {
+		int pos= 0;
+		for (String p:this.placa) {
+			if (p.equalsIgnoreCase(placa))
+				pos = p.indexOf(placa);
 		}
-
-		return 10.0f;
+		return this.horarioEntrada.get(pos);
 	}
 
-	public Object getHorarioSaida(String string) {
-		public String getHorarioSaida(String string) {
-		if (placa == "G49NG" ) {
-			return "9:30";
+	public Float getPercentual(String placa) {
+		int pos= 0;
+		for (String p:this.placa) {
+			if (p.equalsIgnoreCase(placa))
+				pos = p.indexOf(placa);
 		}
-		return "11:00";
+		
+		return this.percentual.get(pos);
+	}
+
+	public String getHorarioSaida(String placa) {
+		int pos= 0;
+		for (String p:this.placa) {
+			if (p.equalsIgnoreCase(placa))
+				pos = p.indexOf(placa);
+		}
+		
+		return this.horarioSaida.get(pos);
 	}
 
 
--- projetoEstacionamento/src/tst/CadastroAcessoHoraCheiaTeste.java
+++ projetoEstacionamento/src/tst/CadastroAcessoHoraCheiaTeste.java
@@ -2,39 +2,61 @@ package tst;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.AcessoHoraCheia;
 
+@RunWith(Parameterized.class)
 public class CadastroAcessoHoraCheiaTeste {
 
 	AcessoHoraCheia acessoHoraCheia;
+	
+	private String placa;
+	private Float valorFracao;
+	private Float percentual;
+	private Float valorAcessoHoraCheia;
+	private String horarioEntrada;
+	private String horarioSaida;
 
+	@Parameters
+	public static List<Object[]> getParameters() {
+		Object[][] parameters = new Object[][] {
+			{"G49NG",30.0f, "8:30", "9:30", 15, 102.0f },
+			{"AC50M",20.0f, "8:00", "11:00", 10, 216.0f },
+			{"AM31J",10.0f, "12:00", "16:00", 0, 40.0f }
+		};
+
+		return Arrays.asList(parameters);
+	}
+
+	public CadastroAcessoHoraCheiaTeste (String placa, float valorFracao, String horarioEntrada, String horarioSaida, float percentual,Float valorAcessoHoraCheia ) {
+		this.placa = placa;
+		this.valorFracao =valorFracao;
+		this.horarioEntrada = horarioEntrada;
+		this.percentual = percentual;
+		this.horarioSaida = horarioSaida;
+		this.valorAcessoHoraCheia = valorAcessoHoraCheia;
+	}
 	@Before
 	public void setup() {
 		acessoHoraCheia = new AcessoHoraCheia();
 	}
 
 	@Test
-	public void test1() {
-		acessoHoraCheia.cadastraAcessoHoraCheia("G49NG",30.0f, "8:30", "9:30", 15);
-		assertTrue(acessoHoraCheia.contemAcessoHoraCheia("G49NG"));
-		assertEquals(acessoHoraCheia.getValorFracao("G49NG"), 30.0f, 0.1f);
-		assertEquals(acessoHoraCheia.getHorarioEntrada("G49NG"), "8:30");
-		assertEquals(acessoHoraCheia.getHorarioSaida("G49NG"),  "9:30");
-		assertEquals(acessoHoraCheia.getPercentual("G49NG"),  15, 0.1f);
-		assertEquals(acessoHoraCheia.getValorAcessoHoraCheia("G49NG"),  102.0f , 0.1f);
-	}
-
-	@Test
-	public void test2() {
-		acessoHoraCheia.cadastraAcessoHoraCheia("AC50M",20.0f, "8:00", "11:00", 10);
-		assertTrue(acessoHoraCheia.contemAcessoHoraCheia("AC50M"));
-		assertEquals(acessoHoraCheia.getValorFracao("AC50M"), 20.0f, 0.1f);
-		assertEquals(acessoHoraCheia.getHorarioEntrada("AC50M"), "8:00");
-		assertEquals(acessoHoraCheia.getHorarioSaida("AC50M"),  "11:00");
-		assertEquals(acessoHoraCheia.getPercentual("AC50M"),  10, 0.1f);
-		assertEquals(acessoHoraCheia.getValorAcessoHoraCheia("AC50M"),  216.0f , 0.1f);
+	public void testCadastraAcessoHoraCheia() {
+		acessoHoraCheia.cadastraAcessoHoraCheia(placa,valorFracao, horarioEntrada, horarioSaida, percentual);
+		assertTrue(acessoHoraCheia.contemAcessoHoraCheia(placa));
+		assertEquals(acessoHoraCheia.getValorFracao(placa), valorFracao, 0.1f);
+		assertEquals(acessoHoraCheia.getHorarioEntrada(placa), horarioEntrada);
+		assertEquals(acessoHoraCheia.getHorarioSaida(placa),  horarioSaida);
+		assertEquals(acessoHoraCheia.getPercentual(placa),  percentual, 0.1f);
+		assertEquals(acessoHoraCheia.getValorAcessoHoraCheia(placa),  valorAcessoHoraCheia, 0.1f);
 	}
 }
