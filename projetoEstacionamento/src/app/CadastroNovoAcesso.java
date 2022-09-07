a2280eb7fbb774b759d4fa571a79c8cd5761de5a
Ítalo Alves <italooko@gmail.com>
2022-09-07 20:51:13 -0300

    Refactoring: Substituir método para metódo objeto. Mau-cheiro identificado: método longo. Descrição da refatoração: aplicou-se a refatoração para transformar o método longo em objeto. Classes/métodos/atributos afetados: * Método novoAcesso() que virou a classe CadastroNovoAcesso.
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

---
+++ projetoEstacionamento/src/app/CadastroNovoAcesso.java
@@ -0,0 +1,55 @@
+package app;
+
+import java.util.Scanner;
+
+public class CadastroNovoAcesso {
+	private Scanner input = new Scanner(System.in);
+	
+    private int veiculosQnt;
+    private Eventos eventos;
+    private int estacionamentoTam;
+
+    public CadastroNovoAcesso(int veiculosQnt, Eventos eventos, int estacionamentoTam) {
+        this.veiculosQnt = veiculosQnt;
+        this.eventos = eventos;
+        this.estacionamentoTam = estacionamentoTam;
+    }
+    
+    public Veiculo computar() {
+        if(this.veiculosQnt == this.estacionamentoTam) {
+            System.out.println("Estacionamento lotado!");
+        } else {
+            if (this.eventos.eventos.size() > 0) {
+                System.out.println("Acesso por evento? (S/n)");
+                String ev = input.next();
+                if(ev.contentEquals("S") || ev.contentEquals("s")){
+                    System.out.println("Selecione o evento: ");
+                    for (int i = 0; i < this.eventos.eventos.size(); i++) {
+                        System.out.printf("%d - %s | Início %s | Fim %s\n", i+1, eventos.eventos.get(i).nome, eventos.eventos.get(i).horaInicio, eventos.eventos.get(i).horaFim);
+                    }
+                    int op = input.nextInt();
+                    
+                    System.out.println("Insira a placa do veículo:");
+                    String pe = input.next();
+                    
+                    Veiculo v = new Veiculo(pe, this.eventos.eventos.get(op-1).horaInicio);
+                    v.ehEvento = true;
+                    v.horario.horario2 = this.eventos.eventos.get(op-1).horaFim;
+                    
+                    System.out.println("Veículo Cadastrado!");
+                    return v;
+                }
+            } else {	
+                System.out.println("Insira a placa do veículo:");
+                String p = input.next();
+                System.out.println("Insira o horário de entrada (HH:mi):");
+                String hi = input.next();
+                
+                Veiculo v = new Veiculo(p, hi);
+                System.out.println("Veículo Cadastrado!");
+                return v;
+            }
+        }
+        return null;
+    }
+}
--- projetoEstacionamento/src/app/Estacionamento.java
+++ projetoEstacionamento/src/app/Estacionamento.java
@@ -272,38 +272,8 @@ public class Estacionamento {
 	}
 
 	private void novoAcesso() throws DescricaoEmBrancoException, ValorAcessoInvalidoException {
-		if(this.veiculos.size() == this.capacidade) {
-			System.out.println("Estacionamento lotado!");
-		} else {
-			if (this.eventos.eventos.size() > 0) {
-				System.out.println("Acesso por evento? (S/n)");
-				String ev = input.next();
-				if(ev.contentEquals("S") || ev.contentEquals("s")){
-					System.out.println("Selecione o evento: ");
-					for (int i = 0; i < this.eventos.eventos.size(); i++) {
-						System.out.printf("%d - %s | Início %s | Fim %s\n", i+1, eventos.eventos.get(i).nome, eventos.eventos.get(i).horaInicio, eventos.eventos.get(i).horaFim);
-					}
-					int op = input.nextInt();
-					
-					System.out.println("Insira a placa do veículo:");
-					String pe = input.next();
-					
-					Veiculo v = new Veiculo(pe, this.eventos.eventos.get(op-1).horaInicio);
-					v.ehEvento = true;
-					v.horario.horario2 = this.eventos.eventos.get(op-1).horaFim;
-					this.veiculos.add(v);
-					System.out.println("Veículo Cadastrado!");
-				}
-			} else {			
-				System.out.println("Insira a placa do veículo:");
-				String p = input.next();
-				System.out.println("Insira o horário de entrada (HH:mi):");
-				String hi = input.next();
-				
-				this.veiculos.add(new Veiculo(p, hi));
-				System.out.println("Veículo Cadastrado!");
-			}
-		}
+		Veiculo novoVeiculo = new CadastroNovoAcesso(this.veiculos.size(), this.eventos, this.capacidade).computar();
+		this.veiculos.add(novoVeiculo);
 		this.menuOpcoes();
 	}
 
